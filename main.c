aab59603e5f46a179b44ad8ca8d782d60e5bdebb
maniHf <manihf@mail.ru>
2023-11-11 00:51:33 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+int 
+main(int argc, char *argv[])
+{
+    if (strcmp(argv[1], "-h") == 0) {
+        printf("\t- yes [argument 1] [argument 2]");}
+    return 0;
+}
